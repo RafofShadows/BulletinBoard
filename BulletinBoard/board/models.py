a06c7ff50d0b1e45ef2465597dd080705de415e3
RafofShadows <pickers97@yandex.ru>
2022-12-25 20:11:30 +0300

    Модели

---
+++ BulletinBoard/board/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from django.db import models
+from tinymce.models import HTMLField
+
+Tank = 'TA'
+Heal = 'HE'
+DD = 'DD'
+Merchant = 'ME'
+Guildmaster = 'GM'
+Questgiver = 'QG'
+Smith = 'BM'
+Tanner = 'LW'
+Alchemist = 'AL'
+Caster = 'SM'
+CATEGORY = [
+    (Tank, 'Танки'),
+    (Heal, 'Хилы'),
+    (DD, 'ДД'),
+    (Merchant, 'Торговцы'),
+    (Guildmaster, 'Гилдмастеры'),
+    (Questgiver, 'Квестгиверы'),
+    (Smith, 'Кузнецы'),
+    (Tanner, 'Кожевники'),
+    (Alchemist, 'Зельевары'),
+    (Caster, 'Мастера заклинаний')
+]
+
+
+# Create your models here.
+class Bulletin(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    header = models.CharField(max_length=100, default='')
+    body = HTMLField()
+    category = models.CharField(max_length=2, choices=CATEGORY, default='DD')
+
+
+class Response(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(default='')
+    bulletin = models.ForeignKey(Bulletin, on_delete=models.CASCADE)
